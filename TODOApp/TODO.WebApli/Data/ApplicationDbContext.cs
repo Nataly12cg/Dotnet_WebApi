8614b9e6a86590dbc867a0c302a168ddeefa20e7
Nataly12cg <natalycorrea99@gmail.com>
2023-10-04 15:38:08 -0500

    first commit

---
+++ TODOApp/TODO.WebApli/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TODO.WebApli.Modelos;
+
+namespace TODO.WebApli.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        // Aquí puedes definir relaciones entre entidades si es necesario.
+    }
+}
+
+
