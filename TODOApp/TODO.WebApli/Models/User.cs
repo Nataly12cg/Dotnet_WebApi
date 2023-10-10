9032e5816dbdbc319465d3c50fc8f9ccf31ecbce
Nataly12cg <144170453+Nataly12cg@users.noreply.github.com>
2023-10-10 16:57:19 -0500

    models de la libreria trabajada con Catalina

--- TODOApp/TODO.WebApli/Models/User.cs
+++ TODOApp/TODO.WebApli/Models/User.cs
@@ -1,19 +1,9 @@
-﻿namespace TODO.WebApli.Modelos
+﻿namespace TODO.WebApi.Models
 {
     public class User
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
-
-        public User()
-        {
-            FirstName = string.Empty;
-            LastName = string.Empty;
-            Email = string.Empty;
-            Password = string.Empty;
-        }
     }
 }
