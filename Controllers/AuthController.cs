94561ad02714a3690c5dd0abb5cc208484c004fc
Nataly12cg <144170453+Nataly12cg@users.noreply.github.com>
2023-11-17 19:02:26 -0500

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MyVaccine.WebApi.Dtos;
+using MyVaccine.WebApi.Literals;
+using MyVaccine.WebApi.Repositories.Contracts;
+
+namespace MyVaccine.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IUserRepository _userRepository;
+    public AuthController(UserManager<IdentityUser> userManager, IUserRepository userRepository)
+    {
+        _userManager = userManager;
+        _userRepository = userRepository;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequetDto model)
+    {
+        var result=await _userRepository.AddUser(model);
+
+       if (!result.Succeeded)
+       {
+            return BadRequest(result.Errors);
+       }
+         return Ok("User registered successfully");   
+  
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+
+        if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+        {
+            var claims = new[]
+            {
+                    new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable(MyVaccineLiterals.JWT_KEY)));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: creds
+            );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            });
+        }
+
+        return Unauthorized();
+    }
+
+}
